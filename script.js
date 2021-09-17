cf088364865142ac3d240291d366dff348916152
LuizFerK <luizfernandoklein@live.com>
2021-09-17 01:02:20 -0300

    feat: Internationalization

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+(function () {
+  localStorage.getItem('@LuizFerK:theme') === 'dark_theme' ?
+    setTheme('dark_theme') : setTheme('light_theme')
+
+  setTimeout(() => {
+    localStorage.getItem('@LuizFerK:lang') === 'en-US' ?
+      setLang('en-US') : setLang('pr-BR')
+  }, 1)
+})()
+
 function setTheme(themeName) {
   localStorage.setItem('@LuizFerK:theme', themeName)
   document.documentElement.className = themeName
-  document.getElementById('toggle').src = themeName === 'dark_theme' ? 'public/sun.svg' : 'public/moon.svg'
+  setTimeout(() => {
+    document.getElementById('toggle').src = themeName === 'dark_theme' ?
+      'public/sun.svg' : 'public/moon.svg'
+  }, 1)
 }
 
 function toggleTheme() {
@@ -9,7 +22,21 @@ function toggleTheme() {
     setTheme('light_theme') : setTheme('dark_theme')
 }
 
-(function () {
-  localStorage.getItem('@LuizFerK:theme') === 'dark_theme' ?
-    setTheme('dark_theme') : setTheme('light_theme')
-})()
+function setLang(lang) {
+  document.documentElement.setAttribute("lang", lang)
+  localStorage.setItem('@LuizFerK:lang', lang)
+
+  if (lang === 'en-US') {
+    document.getElementById('lang_toggle').innerText = "pr-BR"
+    document.getElementById('title').innerText = `Oi, meu nome é\nLuiz Fernando`
+    document.getElementById('description').innerText = "Desenvolvedor Full-Stack. Estudando e me atualizando constantemente. Atualmente estudo Ciência da Computação na UFFS."
+  } else {
+    document.getElementById('lang_toggle').innerText = "en-US"
+    document.getElementById('title').innerText = `Hi, my name is\nLuiz Fernando`
+    document.getElementById('description').innerText = "Full-Stack developer. Studying and constantly updating myself. I'm currently studying Computer Science at UFFS."
+  }
+}
+
+function toggleLang() {
+  document.documentElement.lang === 'en-US' ? setLang('pr-BR') : setLang('en-US')
+}
